3afc1b269b37add235f3320a056e4c0e6bb74373
cgxxv <cghack@yeah.net>
2020-11-17 01:06:05 +0800

    smartpointer example

---
+++ smartpointer/src/main.rs
@@ -0,0 +1,180 @@
+use crate::List::{Cons, Nil};
+use crate::List2::{Cons2, Nil2};
+use crate::List3::{Cons3, Nil3};
+use crate::List4::{Cons4, Nil4};
+
+fn main() {
+    let _ = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    hello(&(*m)[..]);
+
+    let _c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    // _c.drop();
+    drop(_c);
+    println!("CustomSmartPointer dropped before the end of main.");
+
+    // let a = Cons(5,Box::new(Cons(10,Box::new(Nil))));
+    // let _b = Cons(3,Box::new(a));
+    // let _c = Cons(4,Box::new(a));
+
+    let a = Rc::new(Cons2(5, Rc::new(Cons2(10, Rc::new(Nil2)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons2(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons2(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons3(Rc::clone(&value), Rc::new(Nil3)));
+    let b = Cons3(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons3(Rc::new(RefCell::new(10)), Rc::clone(&a));
+    *value.borrow_mut() += 10;
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+
+    let a = Rc::new(Cons4(5, RefCell::new(Rc::new(Nil4))));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+    let b = Rc::new(Cons4(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+    println!("b rc count after changing b = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+    //Uncomment the next line to see that we have a cycle;
+    //it will overflow the stack
+    //println!("a next item = {:?}", a.tail());
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+    }
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+}
+
+struct MyBox<T>(T);
+
+use std::ops::Deref;
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0 //TODO: why???
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use std::rc::Rc;
+
+enum List2 {
+    Cons2(i32, Rc<List2>),
+    Nil2,
+}
+
+use std::cell::RefCell;
+
+#[derive(Debug)]
+enum List3 {
+    Cons3(Rc<RefCell<i32>>, Rc<List3>),
+    Nil3,
+}
+
+#[derive(Debug)]
+enum List4 {
+    Cons4(i32, RefCell<Rc<List4>>),
+    Nil4,
+}
+
+impl List4 {
+    fn tail(&self) -> Option<&RefCell<Rc<List4>>> {
+        match self {
+            Cons4(_, item) => Some(item),
+            Nil4 => None,
+        }
+    }
+}
+
+use std::rc::Weak;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
