83a69ed026cd0cf16e3d51b9c4a71dde018629e6
cgxxv <cghack@yeah.net>
2020-12-04 00:31:55 +0800

    shitcode

--- excel/readme.md
+++ excel/readme.md
@@ -1,4 +1,4 @@
-cargo run demo.xlsx 到货数量 已交数量
+cargo run demo.xlsx 交货数量 已交数量
 
 已交数量=到货数量相加
 欠数=订单数量-- 已交数量
--- excel/src/main.rs
+++ excel/src/main.rs
@@ -1,4 +1,5 @@
 use calamine::{open_workbook_auto, DataType, Reader, Sheets};
+use std::string::String;
 use std::{env, process};
 
 fn main() {
@@ -9,17 +10,20 @@ fn main() {
         process::exit(1);
     });
 
+    let s = String::from("Hello");
+    println!("{:?}", s);
+
     //println!("{:?}", args);
     println!("{:?}", cfg);
 
-    demo(&cfg.file);
+    demo(&cfg);
 }
 
 mod config;
 
-fn demo(path: &str) {
+fn demo(cfg: &config::Config) {
     // opens a new workbook
-    let mut workbook = open_workbook_auto(path).expect("Cannot open file");
+    let mut workbook = open_workbook_auto(&cfg.file).expect("Cannot open file");
 
     // Now get all formula!
     // let sheets = workbook.sheet_names().to_owned();
@@ -27,33 +31,66 @@ fn demo(path: &str) {
     //     range(&s, &mut workbook);
     // }
 
-    range("广灵一斗泉一期", &mut workbook);
+    range("广灵一斗泉一期", &mut workbook, cfg);
 }
 
-fn range(sheet: &str, workbook: &mut Sheets) {
+fn range(sheet: &str, workbook: &mut Sheets, cfg: &config::Config) {
     // Read whole worksheet data and provide some statistics
-    if let Some(Ok(range)) = workbook.worksheet_range(sheet) {
-        let total_cells = range.get_size().0 * range.get_size().1;
-        let non_empty_cells: usize = range.used_cells().count();
-        println!(
-            "Found {} (rows={},cols={}) cells in '{}', including {} non empty cells",
-            total_cells,
-            range.height(),
-            range.width(),
-            sheet,
-            non_empty_cells
-        );
-        // alternatively, we can manually filter rows
-        assert_eq!(
-            non_empty_cells,
-            range
-                .rows()
-                .flat_map(|r| r.iter().filter(|&c| c != &DataType::Empty))
-                .count()
-        );
+    if let Some(Ok(mut range)) = workbook.worksheet_range(sheet) {
+        // let total_cells = range.get_size().0 * range.get_size().1;
+        // let non_empty_cells: usize = range.used_cells().count();
+        // println!(
+        //     "Found {} (rows={},cols={}) cells in '{}', including {} non empty cells",
+        //     total_cells,
+        //     range.height(),
+        //     range.width(),
+        //     sheet,
+        //     non_empty_cells
+        // );
+        // // alternatively, we can manually filter rows
+        // assert_eq!(
+        //     non_empty_cells,
+        //     range
+        //         .rows()
+        //         .flat_map(|r| r.iter().filter(|&c| c != &DataType::Empty))
+        //         .count()
+        // );
+
+        let mut target_index = 0;
+        let mut filter_indexs: Vec<usize> = Vec::new();
 
         for i in 0..range.width() {
-            println!("{} {:?}", i, range.get_value((1, i as u32)));
+            if let Some(subject) = range.get_value((1, i as u32)) {
+                if let Some(s) = subject.get_string() {
+                    let v: Vec<&str> = s.split('\n').collect();
+                    if v.len() == 2 {
+                        if v[1] == cfg.target {
+                            target_index = i;
+                        } else if v[1] == cfg.filter {
+                            filter_indexs.push(i);
+                        }
+                    }
+                    println!("{} {:?} {}", i, v, v.len());
+                }
+            }
+        }
+        println!("{:?}, {:?}", target_index, filter_indexs);
+
+        for row in 2..range.height() {
+            let mut total: f64 = 0.0;
+            for col in filter_indexs.iter() {
+                if let Some(subject) = range.get_value((row as u32, *col as u32)) {
+                    if let Some(f) = subject.get_float() {
+                        total += f;
+                    }
+                }
+            }
+
+            range.set_value((row as u32, target_index as u32), DataType::Float(total));
+            assert_eq!(
+                Some(&DataType::Float(total)),
+                range.get_value((row as u32, target_index as u32))
+            );
         }
     }
 }
