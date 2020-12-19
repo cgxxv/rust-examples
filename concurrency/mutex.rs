a1af989b2f0f4275b82e11163374c9f2b6161bc5
cgxxv <cghack@yeah.net>
2020-12-19 23:32:42 +0800

    shit code

---
+++ concurrency/mutex.rs
@@ -0,0 +1,37 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    single_mutex();
+    multiple_mutex();
+}
+
+fn multiple_mutex() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
+fn single_mutex() {
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
