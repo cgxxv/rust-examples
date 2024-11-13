4772fc044836f3082c95a0d1b07f38a8ba74bdeb
cgxxv <cgsixv@gmail.com>
2024-11-13 20:14:02 +0800

    feat: add smart pointer demo

---
+++ learning/15-6.rs
@@ -0,0 +1,42 @@
+// use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<RefCell<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<Rc<&RefCell<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn main() {
+    let a = RefCell::new(Cons(5, Rc::new(RefCell::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    println!("a next item = {:?}", a.tail());
+}
