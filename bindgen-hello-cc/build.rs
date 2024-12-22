132c68de8581ea10a3d01b4f5126532f83005eef
cgxxv <cgsixv@gmail.com>
2024-12-22 10:37:01 +0800

    update bindgen-hello-cc

--- bindgen-hello-cc/build.rs
+++ bindgen-hello-cc/build.rs
@@ -6,21 +6,15 @@ fn main() {
         .canonicalize()
         .expect("cannot canonicalize path");
 
-    let headers_path = libdir_path.join("hello.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
-
-    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rerun-if-changed={}/*", libdir_path.to_str().unwrap());
 
     // Configure and compile the C code using cc crate
     cc::Build::new()
         .file(libdir_path.join("hello.c"))
         .compile("hello");
 
-    // Tell cargo to look for shared libraries in the specified directory
-    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
-
-    // Tell cargo to tell rustc to link our `hello` library
-    println!("cargo:rustc-link-lib=static=hello");
+    let headers_path = libdir_path.join("hello.h");
+    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
 
     let bindings = bindgen::Builder::default()
         .header(headers_path_str)
@@ -28,6 +22,7 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
+    let out_dir = env::var("OUT_DIR").unwrap();
     let out_path = PathBuf::from(out_dir).join("bindings.rs");
     bindings
         .write_to_file(out_path)
